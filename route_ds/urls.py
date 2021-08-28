8d26b5058417242d716b4b0774d2999d91c891d9
Cesar Garcia <celord@gmail.com>
2021-08-27 19:41:15 -0600

    views and urlpatterns

--- route_ds/urls.py
+++ route_ds/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomePageView
+from .views import RoutedsListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', RoutedsListView.as_view(), name='home')
 ]
--- route_ds/views.py
+++ route_ds/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
+from .models import RouteDs
 
-class HomePageView(TemplateView):
+class RoutedsListView(ListView):
+    model = RouteDs
     template_name = 'home.html'
